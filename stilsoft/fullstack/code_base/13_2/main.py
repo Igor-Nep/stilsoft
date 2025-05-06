ab375117fbe39d76e50cb6dd92b3d10ac08f7be1
Непретимов Игорь Андреевич <nepretimov_i@stilsoft.local>
2025-05-06 13:50:44 +0300

    decorators_half

--- stilsoft/fullstack/code_base/13_2/main.py
+++ stilsoft/fullstack/code_base/13_2/main.py
@@ -3,22 +3,44 @@ class Employee:
     def __init__(self, name, last, pay):
         self.name = name
         self.last = last
-        self.email = f'{self.name}.{self.last}@mail.com'  
-        self.pay = pay
+        self.__pay = pay
 
-    def raise_pay(self):
-        self.pay *= self.raise_coef
+
 
     def f_string(self, data_string):
         self.name, self.surname, self.pay = data_string.split(' ')
         self.pay = int(self.pay)
         return self.name, self.surname, self.pay    
 
-    #@classmethod
-    def from_string(self, data_string):
-        name, surname, pay = data_string.split(' ')
+    @classmethod
+    def from_string(cls, data_string):
+        name, last, pay = data_string.split(' ')
         pay = int(pay)
-        return name,surname,pay
+        return cls(name,last,pay)
+    
+    @classmethod
+    def set_raise_amount(cls, new_coef):
+        cls.raise_coef = new_coef
+
+    @staticmethod
+    def is_workday():
+        from datetime import datetime
+        now = datetime.now()
+        if now.weekday() == 5 or now.weekday() == 6:
+            return False
+        return True
+    
+    @property
+    def email(self):
+        return f'{self.name}.{self.last}@mail.com'
+    
+    @property
+    def pay(self):
+        return self.__pay
+    
+    def raise_pay(self):
+        self.pay *= self.raise_coef
+          
 
 if __name__ == '__main__':
     emp_1 = Employee('Igor', 'Nemov', 50000)
@@ -33,8 +55,21 @@ if __name__ == '__main__':
 
     #testcase 3# New object
 
-    emp_2 = Employee.f_string(data_string='Ivan Boroda 100000')
+    emp_2 = Employee.from_string(data_string='Ivan Boroda 100000')
     assert isinstance(emp_2, Employee)
     assert emp_2.name == 'Ivan'
     assert emp_2.last == 'Boroda'
     assert emp_2.pay == 100000
+
+    #testcase 4 # test raise amount
+    Employee.set_raise_amount(2)
+    assert Employee.raise_coef == 2
+    assert emp_1.raise_coef == 2
+    assert emp_2.raise_coef == 2
+
+    #testcase 5 # is workday
+    assert Employee.is_workday() == True
+
+    #testcase 6 # property get email
+    assert emp_1.email == 'Igor.Nemov@mail.com'
+
--- stilsoft/ssku/main.py
+++ stilsoft/ssku/main.py
@@ -15,16 +15,16 @@ import requests, warnings
 
 
 '''нагрузка atop и docker stats'''
-#Remote('192.168.202.221').docker_logs(write_time=20,cores=48)
-#Remote('192.168.202.200').atop_logs(write_time=10, param='enp3s0')
-#Remote('192.168.202.200').atop_logs(write_time=10, param='enp1s0')
+Remote('192.168.207.69').docker_logs(write_time=20,cores=40)
+#Remote('192.168.207.69').atop_logs(write_time=10, param='eth0')
+#Remote('192.168.207.69').atop_logs(write_time=10, param='eth1')
 #Remote('192.168.202.221').atop_logs(write_time=20, param='eth0')
 ##Remote('192.168.202.221').atop_logs(write_time=20, param='eth1')
 #Remote('192.168.202.221').atop_logs(write_time=20, param='bond0')
 
 
 '''сверка версий'''
-Remote('192.168.207.68').update_versions('ssku')
+#Remote('192.168.207.68').update_versions('ssku')
 #Remote('192.168.202.10').check_versions('ssku')
 #Remote('192.168.207.69').change_versions_modules('ssku')
 
